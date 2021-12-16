00ba5063b86190e62415971e7eb10d275bdcb209
HeidiHmelnitski <92433120+HeidiHmelnitski@users.noreply.github.com>
2021-12-16 13:44:53 +0200

    Add files via upload

---
+++ prog_alused/app.js
@@ -0,0 +1,12 @@
+// muutujad ja andmetüübid
+// 1. deklareerimine
+// let muutujanimi
+// 2. väärtuse omistamine
+// muutujanimi = väärtus
+
+
+let aasta = 2016
+
+let president = 'Kersti Kaljulaid'
+
+console.log(aasta + '.Eesti presidendiks valiti ' + president)
